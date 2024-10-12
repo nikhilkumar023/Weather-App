2328f9348743b6167e661b53c9801e7a2cfe8a7b
nikhilkumar023 <140552089+nikhilkumar023@users.noreply.github.com>
2024-10-12 13:18:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ async function checkWeather(city){
     const api_key = "8370d663182c98cba324ad153b308fd0";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
+    
     const weather_data = await fetch(`${url}`).then(response => response.json());
 
 
